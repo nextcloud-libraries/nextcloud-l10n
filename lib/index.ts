a5c9de143face769de92eee944898b9bd1debc71
Georg Ehrke <developer@georgehrke.com>
2019-10-09 15:58:08 +0200

    export firstDay, dayNames and monthNames
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 /// <reference types="@nextcloud/typings" />
 
-type OC16to17 = Nextcloud.v16.OC | Nextcloud.v17.OC
-declare var OC: OC16to17;
+declare var OC: Nextcloud.v16.OC | Nextcloud.v17.OC | Nextcloud.v18.OC;
+declare var window: Nextcloud.v16.WindowWithGlobals | Nextcloud.v17.WindowWithGlobals | Nextcloud.v18.WindowWithGlobals;
 
 /**
  * Returns the user's locale
@@ -69,4 +69,88 @@ export function translatePlural(app: string, textSingular: string, textPlural: s
     }
 
     return OC.L10N.translatePlural(app, textSingular, textPlural, count, vars, options)
+}
+
+/**
+ * Get the first day of the week
+ *
+ * @return {number}
+ */
+export function getFirstDay(): Number {
+    if (typeof window.firstDay === 'undefined') {
+        console.warn('No firstDay found')
+        return 1
+    }
+
+    return window.firstDay
+}
+
+/**
+ * Get a list of day names (full names)
+ *
+ * @return {String[]}
+ */
+export function getDayNames(): String[] {
+    if (typeof window.dayNames === 'undefined') {
+        console.warn('No dayNames found')
+        return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    }
+
+    return window.dayNames
+}
+
+/**
+ * Get a list of day names (short names)
+ *
+ * @return {String[]}
+ */
+export function getDayNamesShort(): String[] {
+    if (typeof window.dayNamesShort === 'undefined') {
+        console.warn('No dayNamesShort found')
+        return ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.']
+    }
+
+    return window.dayNamesShort
+}
+
+/**
+ * Get a list of day names (minified names)
+ *
+ * @return {String[]}
+ */
+export function getDayNamesMin(): String[] {
+    if (typeof window.dayNamesMin === 'undefined') {
+        console.warn('No dayNamesMin found')
+        return ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+    }
+
+    return window.dayNamesMin
+}
+
+/**
+ * Get a list of month names (full names)
+ *
+ * @return {String[]}
+ */
+export function getMonthNames(): String[] {
+    if (typeof window.monthNames === 'undefined') {
+        console.warn('No monthNames found')
+        return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    }
+
+    return window.monthNames
+}
+
+/**
+ * Get a list of month names (short names)
+ *
+ * @return {String[]}
+ */
+export function getMonthNamesShort(): String[] {
+    if (typeof window.monthNamesShort === 'undefined') {
+        console.warn('No monthNamesShort found')
+        return ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.']
+    }
+    
+    return window.monthNamesShort
 }
