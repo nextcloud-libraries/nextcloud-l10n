adc29f7770a4d8848390b360e8d4cb549f3f960f
John Molakvoæ <skjnldsv@protonmail.com>
2023-01-05 16:12:44 +0100

    fix(tests): Fix jest config
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	clearMocks: true,
+	collectCoverageFrom: ['lib/**/*.ts'],
+	testEnvironment: 'jsdom',
+	preset: 'ts-jest/presets/js-with-ts',
+	transform: {
+		'^.+\\.[tj]sx?$': [
+			'ts-jest', {
+				tsconfig: 'tests/tsconfig.json',
+			},
+		]
+	},
+	transformIgnorePatterns: [],
+}
