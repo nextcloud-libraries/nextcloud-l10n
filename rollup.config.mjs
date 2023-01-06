95a2cdc5ee832dd5ddf096ba573236451fef89da
Ferdinand Thiessen <rpm@fthiessen.de>
2023-01-06 17:50:46 +0100

    Fix ESM build by using rollup
    Use rollup to bundle sources into one output file, fixes an issue where
    importing the `gettext` subpackage is not possible (invalid directory import).
    Also it looks like `.mjs` is required to work with node even if `exports` is set in `package.json`.
    
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

---
+++ rollup.config.mjs
@@ -0,0 +1,37 @@
+import typescript from '@rollup/plugin-typescript'
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+
+const external = [...Object.keys(pkg.dependencies)]
+
+const config = (input, output) => ({
+	input,
+	external,
+	plugins: [
+		typescript({
+            declaration: true,
+        }),
+	],
+	output: [output],
+})
+
+export default [
+	config('./lib/index.ts', {
+		file: 'dist/index.mjs',
+		format: 'esm',
+	}),
+	config('./lib/index.ts', {
+		file: 'dist/index.js',
+		format: 'cjs',
+	}),
+	config('./lib/gettext.ts', {
+		file: 'dist/gettext.mjs',
+		format: 'esm',
+	}),
+	config('./lib/gettext.ts', {
+		file: 'dist/gettext.js',
+		format: 'cjs',
+	})
+]
